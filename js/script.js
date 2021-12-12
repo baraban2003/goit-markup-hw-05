ed7e9c2ff7b395c19ddc804078fee44c8e64c7a0
Volodymyr Baraban <barabanv2003@gmail.com>
2021-12-12 15:00:42 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+window.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+
+    let tab = document.querySelector(".filters"),
+        tabContent = document.querySelector(".filtered-activity");
+
+    tab.addEventListener('click', (e) => {
+        console.log(e.target.classList.item(1));
+
+        let items = tabContent.getElementsByClassName('all');
+        console.log(items);
+
+        for (let i = 0; i < items.length; i++) {
+            if (items[i].classList.contains(e.target.classList.item(1))) {
+                items[i].style.display = 'block';
+            } else {
+                items[i].style.display = 'none';
+            }
+        }
+
+    });
+
+
+
+
+});
